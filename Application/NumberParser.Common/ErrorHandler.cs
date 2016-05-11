dc0bd194a2ccbd28d7fd9bb14f35bfd28952877d
Raffael Hold <raffael@it-hold.de>
2016-05-11 19:55:14 +0200

    Implement ErrorHandler functionality and tests

---
+++ Application/NumberParser.Common/ErrorHandler.cs
@@ -0,0 +1,31 @@
+﻿namespace NumberParser.Common
+{
+	using System.Collections.ObjectModel;
+
+	/// <summary>
+	/// Static class which makes all errors bindable from the view.
+	/// </summary>
+	public static class ErrorHandler
+	{
+		/// <summary>
+		/// Initializes the <see cref="Errors"/> collection
+		/// </summary>
+		static ErrorHandler()
+		{
+			Errors = new ObservableCollection<string>();
+		}
+
+		/// <summary>
+		/// A collection of all errors.
+		/// </summary>
+		public static ObservableCollection<string> Errors { get; set; }
+
+		/// <summary>
+		/// Reset all errors.
+		/// </summary>
+		public static void Reset()
+		{
+			Errors.Clear();
+		}
+	}
+}
---
+++ Tests/NumberParser.Common.Tests/UnitTests/ErrorHandlerTests.cs
@@ -0,0 +1,17 @@
+﻿namespace NumberParser.Common.Tests.UnitTests
+{
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	[TestClass]
+	public class ErrorHandlerTests
+	{
+		[TestMethod]
+		public void ResetClearsTheErrorsCollection()
+		{
+			ErrorHandler.Errors.Add("Test error");
+			ErrorHandler.Reset();
+
+			Assert.IsTrue(ErrorHandler.Errors.Count == 0);
+		}
+	}
+}
