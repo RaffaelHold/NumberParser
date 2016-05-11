60880f14e1ffc50f48f5e56a33f9e96565e56440
Raffael Hold <raffael@it-hold.de>
2016-05-11 10:10:29 +0200

    Implement IDataReader and FileReader

---
+++ Application/NumberParser.Data/DataReader/FileReader.cs
@@ -0,0 +1,40 @@
+﻿namespace NumberParser.Data.DataReader
+{
+	using System.IO;
+	using System.Text;
+
+	public class FileReader : IDataReader
+	{
+		/// <summary>
+		/// Path of the file to be read
+		/// </summary>
+		private string filePath;
+
+		/// <summary>
+		/// Constructs a new <see cref="FileReader"/> and sets its <see cref="filePath"/>
+		/// </summary>
+		/// <param name="filePath">Path to the file to be read</param>
+		public FileReader(string filePath)
+		{
+			this.filePath = filePath;
+		}
+
+		/// <summary>
+		/// Asynchronously reads data from a file.
+		/// </summary>
+		/// <returns>Read data as string array</returns>
+		public string[] ReadData()
+		{
+			try
+			{
+				return File.ReadAllLines(filePath, Encoding.UTF8);
+			}
+			catch (FileNotFoundException ex)
+			{
+				// ToDo: Pass an error message to the view
+
+				return new string[0];
+			}
+		}
+	}
+}
---
+++ Application/NumberParser.Data/DataReader/IDataReader.cs
@@ -0,0 +1,11 @@
+﻿namespace NumberParser.Data.DataReader
+{
+	internal interface IDataReader
+	{
+		/// <summary>
+		/// Asynchronously reads data from a source.
+		/// </summary>
+		/// <returns>Read data as string array</returns>
+		string[] ReadData();
+	}
+}
