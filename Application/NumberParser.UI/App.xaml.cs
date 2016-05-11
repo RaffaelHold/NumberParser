dca8d77ae1fe6d4f93048fab526594905bb8a27f
Raffael Hold <raffael@it-hold.de>
2016-05-11 11:19:29 +0200

    Add dependency injection

--- Application/NumberParser.UI/App.xaml.cs
+++ Application/NumberParser.UI/App.xaml.cs
@@ -1,17 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
-namespace NumberParser
+﻿namespace NumberParser
 {
+	using System.Windows;
+	using Microsoft.Practices.Unity;
+	using MainView;
+
 	/// <summary>
 	/// Interaction logic for App.xaml
 	/// </summary>
 	public partial class App : Application
 	{
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+
+			using (var container = new UnityContainer())
+			{
+				container.RegisterType<MainView.MainView>();
+				container.RegisterType<MainViewModel>();
+
+				var view = container.Resolve<MainView.MainView>();
+				view.MainViewModel = container.Resolve<MainViewModel>();
+
+				view.ShowDialog();
+			}
+		}
 	}
 }
